8a792d72bc5079782455446c1cbb4154a5602e83
Jens Hartfelt <jenshartfelt@Jenss-MacBook-Pro-2.local>
2017-12-05 09:18:38 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync').create(),
+useref = require('gulp-useref'),
+uglify = require('gulp-uglify'),np
+gulpIf = require('gulp-if'),
+postcss = require('gulp-postcss'),
+cssnano = require('cssnano'),
+autoprefixer = require('autoprefixer');
+
+// Compiles sass
+gulp.task('sass', function() {
+  return gulp.src('src/sass/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream())
+})
+
+// Make production build of css
+gulp.task('css', function() {
+  var plugins = [
+    autoprefixer({browsers: ['last 1 version']}),
+    cssnano()
+  ]
+  return gulp.src('src/**/*.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('dist'))
+})
+
+// Copy fonts
+// Only needed if we want to host our own font files.
+// gulp.task('copy-fonts', function() {
+// return gulp.src('src/**/*.otf')
+// .pipe(gulp.dest('dist'))
+// })
+
+gulp.task('image-min', function() {
+
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+      index: 'index.html' // Is only needed if it isn't named index.html
+    },
+  })
+})
+
+gulp.task('js-conc-min', function(){
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('src/*.html', browserSync.reload)
+  gulp.watch('src/sass/**/*.scss', ['sass'])
+})
