4626bf1809af2307ccf91c2b8c83b5c08769441c
Jens Hartfelt <jenshartfelt@Jenss-MacBook-Pro-2.local>
2017-12-08 11:07:52 +0100

    Finished the event-filter layout

--- src/js/event-filters.js
+++ src/js/event-filters.js
@@ -4,10 +4,10 @@ var filterCollapseExpand = {
   },
   toggleCollapseExpand: function(toggleId) {
     var elToToggle = document.querySelector('[data-collapse-id="'+toggleId+'"]')
-    elToToggle.classList.toggle("expanded");
+    elToToggle.classList.toggle("expanded")
   }, 
   addEventListeners: function() {
-    var self = this;
+    var self = this
     for (var i = 0; i < this.el.btnsToggleCollapse.length; i++) {
       this.el.btnsToggleCollapse[i].addEventListener("click", function() {
         var toggleId = this.getAttribute("data-collapse-toggle-id");
@@ -17,7 +17,7 @@ var filterCollapseExpand = {
   },
   init: function() {
     this.el.btnsToggleCollapse = document.getElementsByClassName("btnToggleCollapse")
-    this.addEventListeners();
+    this.addEventListeners()
   }
 }
 window.addEventListener("load", filterCollapseExpand.init.bind(filterCollapseExpand))
