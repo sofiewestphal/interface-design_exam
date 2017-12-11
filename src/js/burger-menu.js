b0b3cd6ba314381c7a2d1248f7da81b24fdc13db
Jens Hartfelt <jenshartfelt@Jenss-MacBook-Pro-2.local>
2017-12-11 22:46:00 +0100

    added burger-menu to all pages. The scss is sort of horrible and could be refactored if we have time

---
+++ src/js/burger-menu.js
@@ -0,0 +1,18 @@
+var burgerMenu = {
+  el: {},
+  open: function() {
+    burgerMenu.el.navBarContainer.classList.add("burger-menu-active")
+  },
+  close: function() {
+    burgerMenu.el.navBarContainer.classList.remove("burger-menu-active")
+  },
+  init: function() {
+    burgerMenu.el.navBarContainer = document.querySelector(".nav-bar")
+    burgerMenu.el.btnToggleOpen = document.querySelector(".burger-menu-toggle.open")
+    burgerMenu.el.btnToggleClose = document.querySelector(".burger-menu-toggle.close")
+    burgerMenu.el.btnToggleOpen.addEventListener("click", burgerMenu.open)
+    burgerMenu.el.btnToggleClose.addEventListener("click", burgerMenu.close)
+    
+  }
+}
+window.addEventListener("DOMContentLoaded", burgerMenu.init);
