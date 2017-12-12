07b00bc0380f936834ef248fa11e69905bda4f64
Christian Thorsø <christian@thorsoe.dk>
2017-12-12 10:21:25 +0100

    Added slider images, fixed a bug in the carousel script and more

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,28 +1,29 @@
 try {
   setCarouselHeight();
+
+   var aCarouselBtns = document.querySelectorAll('.carousel-btns a');
+
+   for (var i = 0; i < aCarouselBtns.length; i++) {
+      var btnCarousel = aCarouselBtns[i];
+      btnCarousel.addEventListener('click', function(e){
+         if(e.currentTarget.classList.contains('previousBtn')){
+            goToPrevImg();
+         }
+         if(e.currentTarget.classList.contains('nextBtn')){
+            goToNextImg();
+         }
+      });
+   }
+   
 } catch (err) {
-  console.log("Error in carousel.js -> No carousel on this page...")
-}
-
-var aCarouselBtns = document.querySelectorAll('.carousel-btns a');
-
-for (var i = 0; i < aCarouselBtns.length; i++) {
-   var btnCarousel = aCarouselBtns[i];
-   btnCarousel.addEventListener('click', function(e){
-      if(e.currentTarget.classList.contains('previousBtn')){
-         goToPrevImg();
-      }
-      if(e.currentTarget.classList.contains('nextBtn')){
-         goToNextImg();
-      }
-   });
+   console.log("Error in carousel.js -> No carousel on this page...")
 }
 
 function goToPrevImg(){
    var elemActiveImage = document.querySelector('.image-carousel .active-image');
-   if(elemActiveImage.previousSibling.localName == "div"){
-      elemActiveImage.previousSibling.classList.add('active-image');
-      elemActiveImage.previousSibling.classList.remove('previous-image');
+   if(elemActiveImage.previousElementSibling != null && elemActiveImage.previousElementSibling.classList.contains('carousel-image')){
+      elemActiveImage.previousElementSibling.classList.add('active-image');
+      elemActiveImage.previousElementSibling.classList.remove('previous-image');
       elemActiveImage.classList.add('upcoming-image');
       elemActiveImage.classList.remove('active-image');
    }
@@ -30,9 +31,9 @@ function goToPrevImg(){
 
 function goToNextImg(){
    var elemActiveImage = document.querySelector('.image-carousel .active-image');
-   if(elemActiveImage.nextSibling.localName == "div"){
-      elemActiveImage.nextSibling.classList.add('active-image');
-      elemActiveImage.nextSibling.classList.remove('upcoming-image');
+   if(elemActiveImage.nextElementSibling != null && elemActiveImage.nextElementSibling.classList.contains('carousel-image')){
+      elemActiveImage.nextElementSibling.classList.add('active-image');
+      elemActiveImage.nextElementSibling.classList.remove('upcoming-image');
       elemActiveImage.classList.add('previous-image');
       elemActiveImage.classList.remove('active-image');
    }
