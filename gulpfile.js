b7fb9fb46501cc1e1d268478082146fbe2f195b0
Jens Hartfelt <jenshartfelt@Jenss-MacBook-Pro-2.local>
2017-12-13 13:30:36 +0100

    implemented new user role, there is not noUser, user and admin + also removed the 'connected to browsersync' message as it annoyed me. Can be turned on again in the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,32 @@
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-browserSync = require('browser-sync').create(),
-useref = require('gulp-useref'),
-uglify = require('gulp-uglify'),
-gulpIf = require('gulp-if'),
-postcss = require('gulp-postcss'),
-cssnano = require('cssnano'),
-autoprefixer = require('autoprefixer'),
-imagemin = require('gulp-imagemin');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create(),
+  useref = require("gulp-useref"),
+  uglify = require("gulp-uglify"),
+  gulpIf = require("gulp-if"),
+  postcss = require("gulp-postcss"),
+  cssnano = require("cssnano"),
+  autoprefixer = require("autoprefixer"),
+  imagemin = require("gulp-imagemin");
 
 // Compiles sass
-gulp.task('sass', function() {
-  return gulp.src('src/sass/**/*.scss')
+gulp.task("sass", function() {
+  return gulp
+    .src("src/sass/**/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream())
-})
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
 
 // Make production build of css
-gulp.task('css-conc-min', function() {
-  var plugins = [
-    autoprefixer({browsers: ['last 1 version']}),
-    cssnano()
-  ]
-  return gulp.src('src/*.html')
+gulp.task("css-conc-min", function() {
+  var plugins = [autoprefixer({ browsers: ["last 1 version"] }), cssnano()];
+  return gulp
+    .src("src/*.html")
     .pipe(useref())
-    .pipe(gulpIf('*.css', postcss(plugins)))
-    .pipe(gulp.dest('dist'))
-})
+    .pipe(gulpIf("*.css", postcss(plugins)))
+    .pipe(gulp.dest("dist"));
+});
 
 // Copy fonts
 // Only needed if we want to host our own font files.
@@ -36,32 +35,35 @@ gulp.task('css-conc-min', function() {
 // .pipe(gulp.dest('dist'))
 // })
 
-gulp.task('image-min', function() {
-  return gulp.src('src/images/*')
+gulp.task("image-min", function() {
+  return gulp
+    .src("src/images/*")
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
-})
+    .pipe(gulp.dest("dist/images"));
+});
 
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function() {
   browserSync.init({
     server: {
-      baseDir: 'src',
-      index: 'index.html' // Is only needed if it isn't named index.html
+      baseDir: "src",
+      index: "index.html" // Is only needed if it isn't named index.html
     },
-  })
-})
+    notify: false
+  });
+});
 
-gulp.task('js-conc-min', function(){
-  return gulp.src('src/*.html')
+gulp.task("js-conc-min", function() {
+  return gulp
+    .src("src/*.html")
     .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
-})
+    .pipe(gulpIf("*.js", uglify()))
+    .pipe(gulp.dest("dist"));
+});
 
-gulp.task('watch', ['browserSync'], function() {
-  gulp.watch('src/*.html', browserSync.reload)
-  gulp.watch('src/sass/**/*.scss', ['sass'])
-  gulp.watch('src/js/**/*.js', ['js-conc-min'])
-  gulp.watch('src/css/**/*.css', ['css-conc-min'])
-  gulp.watch('src/images/*', ['image-min'])
-})
+gulp.task("watch", ["browserSync"], function() {
+  gulp.watch("src/*.html", browserSync.reload);
+  gulp.watch("src/sass/**/*.scss", ["sass"]);
+  gulp.watch("src/js/**/*.js", ["js-conc-min"]);
+  gulp.watch("src/css/**/*.css", ["css-conc-min"]);
+  gulp.watch("src/images/*", ["image-min"]);
+});
